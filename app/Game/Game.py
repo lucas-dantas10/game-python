29853a3f72d9a770f8f87ba5c2a2cc4551c64b11
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-10-03 12:33:02 -0300

    Classe game, onde inicia o jogo

--- app/Game/Game.py
+++ app/Game/Game.py
@@ -1,28 +1,32 @@
 import pygame
 from pygame.locals import *
 from app.Screen.Screen import Screen
+from app.World.World import World
 
 class Game:
 
     def __init__(self, screen_mounted: Screen, screen_surface) -> None:
+        # VARIABLES OF AMBIENT
         self.screen = screen_mounted
         self.screen_surface = screen_surface
+        self.tile_size = 50
         pass
 
 
     def run(self) -> None:
         pygame.init()
 
-        sun_image = pygame.image.load('imgs/sun.png')
-        bg_img = pygame.image.load('imgs/sky.png')
+        world = World(self.tile_size)
 
         run = True 
         while run:
 
-            self.screen_surface.blit(bg_img, (0, 0))
-            self.screen_surface.blit(sun_image, (100, 100))
+            self.add_main_images()
 
-            self.screen.draw_grid(200)
+            world.draw()
+
+            self.screen.draw_grid(self.tile_size)
+
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -31,3 +35,10 @@ class Game:
             pygame.display.update()
 
         pygame.quit()
+
+    def add_main_images(self):
+        sun_image = pygame.image.load('imgs/sun.png')
+        bg_img = pygame.image.load('imgs/sky.png')
+
+        self.screen_surface.blit(bg_img, (0, 0))
+        self.screen_surface.blit(sun_image, (100, 100))
