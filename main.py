97d59ef4286dcc5bc8a5ecc33e014b55083e6d8c
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-10-03 12:32:23 -0300

    Mudança no tamanho da tela do game

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app.Game.Game import Game
 
 def main():
 
-    screen_mounted = Screen(screen_width=1000 ,screen_height=1000)
+    screen_mounted = Screen(screen_width=1000 ,screen_height=600)
     screen = screen_mounted.set_mode()
     
     game = Game(screen_mounted, screen)
