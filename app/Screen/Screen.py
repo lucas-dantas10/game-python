df74c5196935f24903362ff9fc24d7fc8ce742a2
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-10-03 12:33:21 -0300

    Classe Screen, onde fica as configurações da tela

--- app/Screen/Screen.py
+++ app/Screen/Screen.py
@@ -7,16 +7,18 @@ class Screen():
     def __init__(self, screen_width: int, screen_height: int) -> None:
         self.screen_width = screen_width
         self.screen_height = screen_height
+        self.screen_surface = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption('Platformer')
         pass
 
     def set_mode(self) -> Surface:
-        self.screen_surface = pygame.display.set_mode((self.screen_width, self.screen_height))
         return self.screen_surface
     
     def draw_grid(self, tile_size) -> None:
-        for line in range(0, 6):
-            pygame.draw.line(self.screen_surface, (255, 255, 255), (0, line * tile_size), (self.screen_width, line * tile_size))
-            pygame.draw.line(self.screen_surface, (255, 255, 255), (line * tile_size, 0), (line * tile_size, self.screen_height))
-
-
+        color_white = (255, 255, 255)
+        for line in range(0, 20):
+            pygame.draw.line(self.screen_surface, color_white, (0, line * tile_size), (self.screen_width, line * tile_size))
+            pygame.draw.line(self.screen_surface, color_white, (line * tile_size, 0), (line * tile_size, self.screen_height))
+    
+    def get_surface(self):
+        return pygame.display.get_surface()
