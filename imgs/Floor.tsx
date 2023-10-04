a16837b60ca6931fd80a91bb07af0ca4dc43fc24
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2023-10-04 15:36:35 -0300

    Novas imagens

---
+++ imgs/Floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Floor" tilewidth="32" tileheight="32" tilecount="2200" columns="44">
+ <image source="5 - level graphics/graphics/tilemap/Floor.png" width="1408" height="1600"/>
+</tileset>
